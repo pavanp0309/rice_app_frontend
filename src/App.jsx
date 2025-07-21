d3a470da59621708acff7385f6e05bf2b594fd94
pavanp0309 <pavanreddyp0309@gmail.com>
2025-07-21 16:05:08 +0530

    logi_page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './pages/auth/Login'
+
+const App = () => {
+  return (
+    <>
+      <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import "/node_modules/bootstrap/dist/css/bootstrap.css"
+import "/node_modules/bootstrap-icons/font/bootstrap-icons.min.css"
+import "./styles/theme.css"
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
